478cb638c39199e60ed215d79a26d47ac0d4e09a
sjquant <seonujang92@gmail.com>
2023-12-07 01:32:09 +0900

    👷 Vite.config.ts to build web component

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,59 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { transform } from "esbuild";
+import pkg from "./package.json";
 
-// https://vitejs.dev/config/
+const bundleComponents = process.env.BUNDLE_COMPONENTS ?? true;
+
 export default defineConfig({
-  plugins: [svelte()],
-})
+  plugins: [
+    svelte({
+      compilerOptions: {
+        customElement: true,
+      },
+    }),
+    minifyEs(),
+  ],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    lib: {
+      entry: "src/lib/index.ts",
+      formats: bundleComponents ? (["es", "esm", "umd"] as any) : ["es"],
+      name: pkg.name.replace(/-./g, (char) => char[1].toUpperCase()),
+      fileName: (format) =>
+        ({
+          es: `${pkg.name}.js`,
+          esm: `${pkg.name}.min.js`,
+          umd: `${pkg.name}.umd.js`,
+        }[format]),
+    },
+    rollupOptions: {
+      output: bundleComponents
+        ? {}
+        : {
+            inlineDynamicImports: false,
+            chunkFileNames: "[name].js",
+            manualChunks: { svelte: ["svelte"] },
+          },
+    },
+  },
+});
+
+function minifyEs() {
+  return {
+    name: "minifyEs",
+    renderChunk: {
+      order: "post" as const,
+      async handler(code, chunk, outputOptions) {
+        if (
+          outputOptions.format === "es" &&
+          (!bundleComponents || chunk.fileName.endsWith(".min.js"))
+        ) {
+          return await transform(code, { minify: true });
+        }
+        return code;
+      },
+    },
+  };
+}
